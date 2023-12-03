0cc7197f1224886ebe39f33f8861183ff15dc410
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2023-12-03 12:35:04 +0100

    .

---
+++ task-2.js
@@ -0,0 +1,27 @@
+class Storage {
+  constructor(items) {
+    this._items = items;
+  }
+
+  getItems() {
+    return this._items;
+  }
+
+  addItem(newItem) {
+    this._items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const itemIndex = this._items.indexOf(itemToRemove);
+    if (itemIndex !== -1) {
+      this._items.splice(itemIndex, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
